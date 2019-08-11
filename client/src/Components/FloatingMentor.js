4be36aa72e755db234d5c1a87fd37d4038377d85
ahmet <ahmet.kio@hotmail.com>
2019-08-11 12:01:59 +0100

    reviewing

--- client/src/Components/FloatingMentor.js
+++ client/src/Components/FloatingMentor.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import moment from "moment"
+import moment from "moment";
 import {
   Button,
   Pane,
@@ -13,9 +13,7 @@ import {
   Textarea
 } from "evergreen-ui";
 import "../App.css";
-// import mockStudentsProfiles from "../mockStudentsProfiles.json";
-import {getStudents, getSkills} from "./Api";
-import { ThemeConsumer } from "evergreen-ui/commonjs/theme";
+import { getStudents, getSkills } from "./Api";
 
 const selectedFloatingMentorName = "Maria";
 
@@ -52,8 +50,7 @@ export class FloatingMentor extends Component {
     });
   };
 
-  handyStudent = (selected) => {
-    console.log("handystudent working....");
+  handyStudent = selected => {
     const selectedStudentProfile = this.state.studentsProfile.find(s => {
       return s.name === selected;
     });
@@ -62,8 +59,7 @@ export class FloatingMentor extends Component {
       errormessage: null,
       selectedStudentProfile: selectedStudentProfile
     });
-
-     };
+  };
 
   handyComments = async e => {
     e.preventDefault();
@@ -74,38 +70,35 @@ export class FloatingMentor extends Component {
             FloatingMentorComments: this.state.commentSubmitted
           });
     }
- 
-     
-     if (this.state.moduleSelected !== null) {
-     try {
-      const res = await fetch("api/updateComments", {
-        method: "PUT",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          name: this.state.clickedStudent,
-          floatingmentorcomment: this.state.commentSubmitted,
-          floatingMentorName: selectedFloatingMentorName,
-          selectedmodule: this.state.moduleSelected
-        })
-      });
-      // console.log("fetch:", body);
-      const json = await res.json();
-      if (res.status !== 200) {
-        alert(json.msg);
-      } 
-      const student = await res;
 
-      this.setState({
-        studentprofile: student,
-      selectedStudentProfile: selectedStudentProfile
-      });
-    } catch (err) {
-      console.log(err);
+    if (this.state.moduleSelected !== null) {
+      try {
+        const res = await fetch("api/updateComments", {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            name: this.state.clickedStudent,
+            floatingmentorcomment: this.state.commentSubmitted,
+            floatingMentorName: selectedFloatingMentorName,
+            selectedmodule: this.state.moduleSelected
+          })
+        });
+
+        const json = await res.json();
+        if (res.status !== 200) {
+          alert(json.msg);
+        }
+        const student = await res;
+
+        this.setState({
+          studentprofile: student,
+          selectedStudentProfile: selectedStudentProfile
+        });
+      } catch (err) {}
     }
-  }
   };
 
   handleError = () => {
@@ -124,12 +117,9 @@ export class FloatingMentor extends Component {
 
   render() {
     const studentsProfile = this.state.studentsProfile;
-    const selectedStudentProfile = this.state.selectedStudentProfile;
     return (
       <div>
         <Pane
-          // key={index}
-          // key={index}
           elevation={4}
           height="100%"
           width="auto"
@@ -152,7 +142,6 @@ export class FloatingMentor extends Component {
             <Heading is="h2">Select student</Heading>
 
             <Combobox
-
               items={studentsProfile.map(s => s.name)}
               height={34}
               onChange={selected => this.handyStudent(selected)}
@@ -187,18 +176,10 @@ export class FloatingMentor extends Component {
                 />
                 <Table.Body>
                   <Table.Head>
-                    <Table.TextCell
-                      flexBasis={100}
-                      flexShrink={0}
-                      flexGrow={0}
-                    >
+                    <Table.TextCell flexBasis={100} flexShrink={0} flexGrow={0}>
                       Name:
                     </Table.TextCell>
-                    <Table.TextCell
-                      flexBasis={100}
-                      flexShrink={0}
-                      flexGrow={0}
-                    >
+                    <Table.TextCell flexBasis={100} flexShrink={0} flexGrow={0}>
                       {this.state.selectedStudentProfile &&
                         this.state.selectedStudentProfile.name}
                     </Table.TextCell>
@@ -356,7 +337,8 @@ export class FloatingMentor extends Component {
                                   </Paragraph>
                                   <Text padding={8} alignSelf="flex-end">
                                     <Strong>Feedback by </Strong>
-                                    {s.floatingMentorName} on {moment(s.date).format("Do MMMM  YYYY")}{" "}
+                                    {s.floatingMentorName} on{" "}
+                                    {moment(s.date).format("Do MMMM  YYYY")}{" "}
                                     <br />
                                     <Text padding={8}>
                                       {" "}
@@ -378,7 +360,6 @@ export class FloatingMentor extends Component {
                 marginLeft={20}
                 padding={10}
                 display="flex"
-                // flexDirection= "column"
                 justifyContent="space-around"
                 borderRadius={3}
                 elevation={2}
@@ -397,18 +378,13 @@ export class FloatingMentor extends Component {
                   <Combobox
                     items={this.state.techSkills.map(s => s.module)}
                     height={34}
-                    onChange={selected =>
-                      this.handleModuleSelection(selected)
-                    }
+                    onChange={selected => this.handleModuleSelection(selected)}
                     placeholder="Select Module"
                     autocompleteProps={{
                       title: "Select Module"
                     }}
                   />
-                  <Heading color="#FF0000">
-                    {" "}
-                    {this.state.errormessage}
-                  </Heading>
+                  <Heading color="#FF0000"> {this.state.errormessage}</Heading>
                   <Textarea
                     height={200}
                     width={400}
